f34905bb5a019c7ff288250e9f32764aa957ed7d
pshrest001 <pshrest001@citymail.cuny.edu>
2019-04-11 05:33:01 -0400

    Add files via upload

---
+++ src/javaconnect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author praveenathegreat
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect {
+    Connection conn=null;
+    ResultSet rs;
+    PreparedStatement pst;
+    
+    public static Connection ConnecrDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\praveenathegreat\\NetbeansProjects\\Employee_Management_System\\ems.sqlite");
+            return conn;
+  
+            
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+    
+}
